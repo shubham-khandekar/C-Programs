b31b290a05c4f9379082026ad2d9ab7c8034a2bd
shubham <shubhamkhandekar001@gmail.com>
2020-12-10 20:52:16 +0530

    Initial Commit c++

---
+++ code_with_harry.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+//////////////////////////////////////////////////////////////
+/*
+int main(){   // this is tutorial 4
+
+    int a = 4,c;
+    int b = 5;
+    float pi = 3.14;
+    cout<<"/////////////////////////////////////////////"<<endl;
+    cin>>c;
+    cout<<"value of a is "<<a<<" and value of b is "<<b<<endl;
+    cout<<"and the value of pi is "<<pi<<endl;
+    return 0;
+
+}
+*/
+
+
+////////////////// BASIC INPUT OUTPUT & MORE LEC 5//////////////////
+/*
+int main()
+{
+    int num1,num2;
+    cout<<"enter the value of num1\n"; //"<<"is called insertion operator
+    cin>>num1; // ">>" extraction operator
+    cout<<"enter the value of num2\n"; //this is called insertion operator
+    cin>>num2; //extraction operator
+
+    cout<<"the sum is "<<num1+num2<<endl;
+    return 0;
+}
+*/
+
+
+////////////////// HEADER FILES & OPERATORS LEC 6/////////////////
+/*
+int main()
+{
+    int a=5,b=6;
+    //there are 2 types of header files
+    // 1. system header files: it comes with the compiler 
+    // 2. user defined header files: It is written by the programmer
+    
+    // cout<<"this is hello world program"<<endl;
+    cout<<"operators in c++"<<endl;
+    cout<<"followinf are the types of operators in c++"<<endl;
+
+    // Arithmetic operators
+    cout<<"the valuie of a+b is "<<a+b<<endl;
+    cout<<"the valuie of a-b is "<<a-b<<endl;
+    cout<<"the valuie of a*b is "<<a*b<<endl;
+    cout<<"the valuie of a/b is "<<a/b<<endl;
+    cout<<"the valuie of a%b is "<<a%b<<endl;
+    cout<<"the valuie of a++ is "<<a++<<endl;
+    cout<<"the valuie of a-- is "<<a--<<endl;
+    cout<<"the valuie of ++a is "<<++a<<endl;
+    cout<<"the valuie of --a is "<<--a<<endl;
+    cout<<endl;
+
+    // Assignment Operators --> used to assigned values to variables
+    // int a=3,b=9;
+    // char d='d';
+
+    // Comparison Operators
+    cout<<"the valuie of a==b is "<<(a==b)<<endl;
+    cout<<"the valuie of a!=b is "<<(a!=b)<<endl;
+    cout<<"the valuie of a>b is "<<(a>b)<<endl;
+    cout<<"the valuie of a<b is "<<(a<b)<<endl;
+    cout<<"the valuie of a<=b is "<<(a<=b)<<endl;
+    cout<<"the valuie of a>=b is "<<(a>=b)<<endl;
+    cout<<endl;
+
+    // Logical Operators
+    cout<<"the valuie of (a==b)&&(a>b)) is "<<((a==b)&&(a>b))<<endl;
+    cout<<"the valuie of (a==b)||(a<b)) is "<<((a==b)||(a<b))<<endl;
+    cout<<"the valuie of !(a==b) is "<<!(a==b)<<endl;
+
+    return 0;
+
+}
+*/
+
+/////////////////
+
+
+
+
+
+
+
+
